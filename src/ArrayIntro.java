ca286dfc0c66dcac02e818ef80c36efd17630cba
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-03-12 23:58:21 +0500

    'Arrays'

---
+++ src/ArrayIntro.java
@@ -0,0 +1,19 @@
+
+public class ArrayIntro {
+
+	public static void main(String[] args) {
+	//	int[] grades = new int[10]; //10 indicates the size of array
+		
+		// grades[0] = 10 ; // indexing in array starts from zero
+		
+		// another methods 
+		int[] grades = {10,9, 8,7 ,6,5,4,3,2,1};
+		
+		//updating value of array
+		grades[1] = 900;
+		
+		System.out.println(grades[1]);
+
+	}
+
+}
