9828bf7123c0d80f12f5261942aeffb13ff08178
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-03-20 23:05:23 +0500

    'sortnReverse'

---
+++ src/SortandReverseList.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SortandReverseList {
+
+	public static void main(String[] args) {
+		
+		List<Integer> allGrades = Arrays.asList(5,3,2,6,3);
+		Collections.sort(allGrades);
+		Collections.reverse(allGrades);
+		
+		for(int grade : allGrades) {
+			System.out.println(grade);
+		}
+
+	}
+
+}
